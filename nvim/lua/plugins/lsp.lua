638039568c9e17600120784fc054dfef27bc6473
yan2k <23281397+yan2k@users.noreply.github.com>
2026-06-01 18:25:13 +0200

    new ps1 + working neovim config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,31 +3,26 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = { "nvim-telescope/telescope.nvim" },
     config = function()
-      vim.lsp.config("lua_ls", {
-        settings = {
-          Lua = {
-            runtime = { version = "LuaJIT" },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false,
+      local lspconfig = require('lspconfig')
+
+      local custom = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              runtime = { version = "LuaJIT" },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+              },
+              diagnostics = { globals = { "vim" } },
             },
-            diagnostics = { globals = { "vim" } },
           },
         },
-      })
+      }
 
-      -- Add/remove servers here as you install them via pacman
-      vim.lsp.enable({
-        "clangd",        -- pacman: clang
-        "gopls",         -- pacman: gopls
-        "pyright",       -- pacman: pyright
-        "lua_ls",        -- pacman: lua-language-server
-        "bashls",        -- pacman: bash-language-server
-        "tinymist",      -- pacman: tinymist
-        "svelte",        -- pacman: svelte-language-server
-        "tailwindcss",   -- pacman: tailwindcss-language-server
-        "rust_analyzer", -- pacman: rust-analyzer
-      })
+      for _, name in ipairs(lspconfig.util.available_servers()) do
+        lspconfig[name].setup(custom[name] or {})
+      end
     end,
   },
 }
